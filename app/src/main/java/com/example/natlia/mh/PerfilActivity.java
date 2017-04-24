0e3313f951736063649f7ec8a4064edc972253bc
Natalia Goldberg <nataliagoldbergnr@gmail.com>
2017-04-23 21:24:34 -0300

    elementos da tela de perfil do hotel setados

--- app/src/main/java/com/example/natlia/mh/PerfilActivity.java
+++ app/src/main/java/com/example/natlia/mh/PerfilActivity.java
@@ -2,6 +2,10 @@ package com.example.natlia.mh;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 public class PerfilActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class PerfilActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
+
+        ImageView imghotel = (ImageView) findViewById(R.id.imageViewFotoHotel);
+        TextView txtnomehotel = (TextView) findViewById(R.id.textViewNomeHotel);
+        RatingBar avaliacao = (RatingBar) findViewById(R.id.ratingBar2);
+        TextView descricao = (TextView) findViewById(R.id.textViewDescricaoHotel);
+        TextView informacoes = (TextView) findViewById(R.id.textViewInfo);
+        TextView contato = (TextView) findViewById(R.id.textViewContato);
+        TextView infocontato = (TextView) findViewById(R.id.textViewInfoContato);
+        ImageView imgmapa = (ImageView) findViewById(R.id.imageView3);
+        Button btnrota = (Button) findViewById(R.id.buttonRota);
+        Button btnligar = (Button) findViewById(R.id.btnLigar);
     }
 }
